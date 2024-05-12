ed90c3b19fde0ea3620be23a9967a34dca355caf
Daniel Evans <daniel.evans@dyedurham.com>
2024-05-13 09:02:52 +1000

    feat: add html form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,36 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'email-ui';
+  to: string = '';
+  subject: string = '';
+  message: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  sendData() {
+    const data = {
+      to: this.to,
+      subject: this.subject,
+      message: this.message
+    }
+
+    this.http.post('backend-url', data).subscribe(response => {
+      console.log('Message sent!', response);
+
+      this.to = '';
+      this.subject = '';
+      this.message = '';
+    })
+  }
 }
